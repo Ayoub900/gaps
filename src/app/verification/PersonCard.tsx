df9586979b12ad52b1b28152802f4c77b598f2e2
Ayoub900 <k.kassimi9009@gmail.com>
2024-10-29 19:36:03 +0100

    add vae accriditaion type

--- src/app/verification/PersonCard.tsx
+++ src/app/verification/PersonCard.tsx
@@ -47,8 +47,9 @@ export default async function PersonCard({ q }: CardProps) {
     return (
 
         <div className='flex flex-col justify-center gap-4 bg-white mx-auto min-w-[300px] w-full max-w-[560px] shadow-lg '>
-            <Image placeholder="blur" blurDataURL="data:..." className='mx-auto object-contain' width={700} height={500} alt={"id"} src={accreditation.imageUrl} />
-
+            {accreditation.type !== "VAE" &&
+                <Image placeholder="blur" blurDataURL="data:..." className='mx-auto object-contain' width={700} height={500} alt={"id"} src={accreditation.imageUrl} />
+            }
             {accreditation.diploma && accreditation.type !== "I" &&
                 <p className='p-4 font-semibold'>
                     has completed the necessary training and assessment to demonstrate competence and understanding within this field. The International Education ASB-Academy awards this certificate in recognition of the achievement <span className='text-red-500'>{accreditation.diploma}</span>
@@ -64,6 +65,15 @@ export default async function PersonCard({ q }: CardProps) {
                         <h1 key={index}>- {d}</h1>
                     ))}
                 </p>
+            }
+            {accreditation.diploma && accreditation.type === "VAE" &&
+                <>
+                    <h1 className='text-3xl font-semibold'>Verified</h1>
+                    <h1>Mr/Ms {accreditation.name}, CIN: {accreditation.cin}</h1>
+                    <p className='p-4 font-semibold'>
+                        Based on the evaluation committee&apos;s review of all relevant documents pertaining to the student&apos;s experience, he has been awarded a &quot;{accreditation.diploma}&quot;
+                    </p>
+                </>
             }
         </div>
     )
