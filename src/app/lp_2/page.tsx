865d35ae92bf6f1dcd01cfcc6da67a7746ee21f9
Ayoub900 <k.kassimi9009@gmail.com>
2024-11-14 15:01:20 +0100

    add facebook pixel to ld_2

--- src/app/lp_2/page.tsx
+++ src/app/lp_2/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import Form from './Form'
+import Script from 'next/script'
 
 interface lp1PageProps {
     searchParams: {
@@ -14,6 +15,20 @@ export default function page({
 }: lp1PageProps) {
     return (
         <div className='bg-red-700'>
+            <Script id="facebook-pixel" strategy="lazyOnload">
+                {`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '8973872249317652');
+                    fbq('track', 'PageView');
+                `}
+            </Script>
             <div className='py-4 flex justify-center w-full bg-white'>
                 <h1 className='text-xl mx-auto font-semibold text-red-600'>ترحب بكم ASB-Academy مؤسسة
                 </h1>
